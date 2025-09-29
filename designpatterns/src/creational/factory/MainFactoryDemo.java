38279a8a745ecc8fdb147ec17231f6fb0a1b5a78
Samyuktha-tech <samyuvenkat83@gmail.com>
2025-09-29 17:01:20 +0530

    Add files via upload

---
+++ designpatterns/src/creational/factory/MainFactoryDemo.java
@@ -0,0 +1,13 @@
+package creational.factory;
+
+public class MainFactoryDemo {
+    public static void main(String[] args) {
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape shape1 = factory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = factory.getShape("RECTANGLE");
+        shape2.draw();
+    }
+}
---
+++ designpatterns/src/creational/factory/Rectangle.java
@@ -0,0 +1,7 @@
+package creational.factory;
+
+public class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
